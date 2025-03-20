821b0121963b8ab90890745aaf27faa30614aa60
Rajuan Ahmed <mail.geekz@proton.me>
2025-03-21 05:41:09 +0600

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,10 +1,9 @@
-export const firebaseConfig = {
-	apiKey: "sjhHDJsh82hJHdh3jHJDHJQk4398",
-	authDomain: "yourdomain.firebaseapp.com",
-	databaseURL: "https://jgjfhg453EHQHT.asia-east1.firebasedatabase.app",
-	projectId: "your project id",
-	storageBucket: "projectid.appspot.com",
-	messagingSenderId: "333747474747477",
-	appId: "yourappid",
-	measurementId: "R-FKJHSJW",
+const firebaseConfig = {
+  apiKey: "AIzaSyBtBeXvdffAS4tj4FoACF_wIW47-HX02uY",
+  authDomain: "anigeek-4f65f.firebaseapp.com",
+  projectId: "anigeek-4f65f",
+  storageBucket: "anigeek-4f65f.firebasestorage.app",
+  messagingSenderId: "1020929402459",
+  appId: "1:1020929402459:web:058a8f65c996cbcebd817e",
+  databaseURL: "https://anigeek-4f65f.asia-south1.firebasedatabase.app"
 };
