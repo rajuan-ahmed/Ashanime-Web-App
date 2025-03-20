58ff3556cccb909ab4b390d590760b60a9da23de
Rajuan Ahmed <mail.geekz@proton.me>
2025-03-21 05:12:29 +0600

    Create firebaseConfig.js

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+	apiKey: "sjhHDJsh82hJHdh3jHJDHJQk4398",
+	authDomain: "yourdomain.firebaseapp.com",
+	databaseURL: "https://jgjfhg453EHQHT.asia-east1.firebasedatabase.app",
+	projectId: "your project id",
+	storageBucket: "projectid.appspot.com",
+	messagingSenderId: "333747474747477",
+	appId: "yourappid",
+	measurementId: "R-FKJHSJW",
+};
